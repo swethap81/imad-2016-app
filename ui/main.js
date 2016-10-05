4a5221af0e02876a12cff1769afaad774eb6a0b5
swethap81 <swethapalle81@gmail.com>
2016-10-05 16:45:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,9 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 
 element.innerHTML = 'New value';
+
+//move the image
+var image = document.getElementById('img');
+img.onclick = function() {
+    img.style.marginLeft = '100px';
+};
